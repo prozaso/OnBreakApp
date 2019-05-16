5a9cc76423c27e6f4c8fae1a065a1889ecc0d0a2
skois <pablo.rozas.omar@gmail.com>
2019-05-16 02:32:56 -0800

    16-05-19/02:32

--- OnBreakLibrary/Cliente.cs
+++ OnBreakLibrary/Cliente.cs
@@ -8,5 +8,35 @@ namespace OnBreakLibrary
 {
     public class Cliente
     {
+
+        private int _rut;
+        private String _razonSocial;
+        private String _nombre;
+        private String _mail;
+        private String _direccion;
+        private String _telefono;
+        private int _actividadEmpresa;
+        private int _tipoEmpresa;
+
+        public Cliente(int rut, string razonSocial, string nombre, string mail, string direccion, string telefono, int actividadEmpresa, int tipoEmpresa)
+        {
+            _rut = rut;
+            _razonSocial = razonSocial;
+            _nombre = nombre;
+            _mail = mail;
+            _direccion = direccion;
+            _telefono = telefono;
+            _actividadEmpresa = actividadEmpresa;
+            _tipoEmpresa = tipoEmpresa;
+        }
+
+        public int Rut { get => _rut; set => _rut = value; }
+        public string RazonSocial { get => _razonSocial; set => _razonSocial = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Mail { get => _mail; set => _mail = value; }
+        public string Direccion { get => _direccion; set => _direccion = value; }
+        public string Telefono { get => _telefono; set => _telefono = value; }
+        public int ActividadEmpresa { get => _actividadEmpresa; set => _actividadEmpresa = value; }
+        public int TipoEmpresa { get => _tipoEmpresa; set => _tipoEmpresa = value; }
     }
 }
--- OnBreakLibrary/Contrato.cs
+++ OnBreakLibrary/Contrato.cs
@@ -9,5 +9,55 @@ namespace OnBreakLibrary
     public class Contrato
     {
 
+        private int _numero;
+        private DateTime _creacionContrato;
+        private DateTime _terminoContrato;
+        private DateTime _inicioEvento;
+        private DateTime _terminoEvento;
+        private String _horaInicio;
+        private String _horaTermino;
+        private String _direccion;
+        private bool _vigente;
+        private int _tipo;
+        private String _observaciones;
+        private Cliente _cliente;
+        private int _asistentes;
+        private int _personalAdicional;
+        private double _valorEvento;
+
+        public Contrato(int numero, DateTime creacionContrato, DateTime terminoContrato, DateTime inicioEvento, DateTime terminoEvento, string horaInicio, string horaTermino, string direccion, bool vigente, int tipo, string observaciones, Cliente cliente, int asistentes, int personalAdicional, double valorEvento)
+        {
+            _numero = numero;
+            _creacionContrato = creacionContrato;
+            _terminoContrato = terminoContrato;
+            _inicioEvento = inicioEvento;
+            _terminoEvento = terminoEvento;
+            _horaInicio = horaInicio;
+            _horaTermino = horaTermino;
+            _direccion = direccion;
+            _vigente = vigente;
+            _tipo = tipo;
+            _observaciones = observaciones;
+            _cliente = cliente;
+            _asistentes = asistentes;
+            _personalAdicional = personalAdicional;
+            _valorEvento = valorEvento;
+        }
+
+        public int Numero { get => _numero; set => _numero = value; }
+        public DateTime CreacionContrato { get => _creacionContrato; set => _creacionContrato = value; }
+        public DateTime TerminoContrato { get => _terminoContrato; set => _terminoContrato = value; }
+        public DateTime InicioEvento { get => _inicioEvento; set => _inicioEvento = value; }
+        public DateTime TerminoEvento { get => _terminoEvento; set => _terminoEvento = value; }
+        public string HoraInicio { get => _horaInicio; set => _horaInicio = value; }
+        public string HoraTermino { get => _horaTermino; set => _horaTermino = value; }
+        public string Direccion { get => _direccion; set => _direccion = value; }
+        public bool Vigente { get => _vigente; set => _vigente = value; }
+        public int Tipo { get => _tipo; set => _tipo = value; }
+        public string Observaciones { get => _observaciones; set => _observaciones = value; }
+        public Cliente Cliente { get => _cliente; set => _cliente = value; }
+        public int Asistentes { get => _asistentes; set => _asistentes = value; }
+        public int PersonalAdicional { get => _personalAdicional; set => _personalAdicional = value; }
+        public double ValorEvento { get => _valorEvento; set => _valorEvento = value; }
     }
 }
